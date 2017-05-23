1bff199b838e35d7b11c73322d37c3b26bce8c15
Igor Kuzmenko <igor@kuzzmi.com>
2017-05-23 23:59:16 +0200

    Adds .extend files and touchpad init script

---
+++ .local/bin/touchpad.sh
@@ -0,0 +1,3 @@
+xinput set-prop 'DLL07BE:01 06CB:7A13 Touchpad' 'libinput Tapping Enabled' 1
+xinput set-prop 'DLL07BE:01 06CB:7A13 Touchpad' 'libinput Natural Scrolling Enabled' 1
+xinput set-prop 'DLL07BE:01 06CB:7A13 Touchpad' 'libinput Click Method Enabled' 0 1

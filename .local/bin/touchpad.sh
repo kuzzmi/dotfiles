d6ac3b9fb643a5cadce90bb67fd1f4012ed40da7
Igor Kuzmenko <igor@kuzzmi.com>
2017-06-06 00:48:32 +0200

    Adds new scripts, removes previous

--- .local/bin/touchpad.sh
+++
@@ -1,3 +0,0 @@
-xinput set-prop 'DLL07BE:01 06CB:7A13 Touchpad' 'libinput Tapping Enabled' 1
-xinput set-prop 'DLL07BE:01 06CB:7A13 Touchpad' 'libinput Natural Scrolling Enabled' 1
-xinput set-prop 'DLL07BE:01 06CB:7A13 Touchpad' 'libinput Click Method Enabled' 0 1

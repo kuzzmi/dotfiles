5f10485417b66660e9e5d13c610c7091708c0191
Igor Kuzmenko <igor@kuzzmi.com>
2017-11-24 15:31:54 +0100

    Adds screenlayout configuration

---
+++ .screenlayout/laptop.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+xrandr --output VIRTUAL1 --off --output eDP1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP1 --off --output HDMI2 --off --output HDMI1 --off --output DP1-3 --off --output DP1-2 --off --output DP1-1 --off --output DP2 --off
+xset r rate 250 50
+setxkbmap -model pc104 -layout us,ua,ru -variant colemak,winkeys,winkeys -option grp:shifts_toggle -option caps:escape

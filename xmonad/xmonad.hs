08162217e84f9ed24e921073c1ff86de578d2dea
Igor Kuzmenko <igor@kuzzmi.com>
2018-07-17 09:36:32 +0300

    Updates XMonad configuration

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,4 +1,8 @@
 import XMonad
+
+import XMonad.Actions.WithAll
+
+import XMonad.Hooks.Place
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 
@@ -6,6 +10,7 @@ import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
 import XMonad.Util.Scratchpad
 
+import XMonad.Layout.Spacing
 import XMonad.Layout.MultiToggle
 import XMonad.Layout.MultiToggle.Instances
 import XMonad.Layout.Grid
@@ -14,59 +19,74 @@ import System.IO
 
 import qualified XMonad.StackSet as W
 
-myTerminal = "alacritty"
+myTerminal           = "alacritty"
+myMask               = mod4Mask        -- win key
+myScratchpadTerminal = "urxvt"
+myPlacement          = fixed (0.5,0.5) -- center of the screen
 
 myManageHook =
-    composeAll
-        [ manageScratchPad
-        ] <+> manageHook def
+    placeHook myPlacement
+        <+> composeAll
+            [ className =? "Pavucontrol" --> doFloat
+            , className =? "Seahorse"    --> doFloat
+            ]
+        <+> manageScratchPad
 
 manageScratchPad :: ManageHook
 manageScratchPad = scratchpadManageHook (W.RationalRect l t w h)
     where
-        h = 0.1     -- terminal height, 10%
-        w = 1       -- terminal width, 100%
-        t = 1 - h   -- distance from top edge, 90%
-        l = 1 - w   -- distance from left edge, 0%
+        h = 1/2   -- terminal height, 50%
+        w = 1/2   -- terminal width, 50%
+        t = 1/4   -- distance from top edge, 25%
+        l = 1/4   -- distance from left edge, 25%
 
 myLayout =
     mkToggle (NOBORDERS ?? FULL ?? EOT)
-    $ Grid ||| tiled ||| Mirror tiled
+    $ tiled ||| grid
     where
+        gaps    = smartSpacingWithEdge 10
+        grid    = gaps $ Grid
         -- default tiling algorithm partitions the screen into two panes
-        tiled   = Tall nmaster delta ratio
+        tiled   = gaps $ Tall nmaster delta ratio
         -- The default number of windows in the master pane
         nmaster = 1
         -- Default proportion of screen occupied by master pane
         ratio   = 1/2
         -- Percent of screen to increment by when resizing panes
-        delta   = 2/100
+        delta   = 1/25
 
 myKeys =
     -- Launch browser
-    [ ((mod4Mask, xK_F2), spawn "inox --force-device-scale-factor=1.5")
+    [ ((myMask, xK_F2), spawn "inox --force-device-scale-factor=1.5")
     -- Applications menu
-    , ((mod4Mask, xK_Tab), spawn "rofi -show combi")
+    , ((myMask, xK_Tab), spawn "rofi -show combi")
     -- Kill focused
-    , ((mod4Mask, xK_BackSpace), kill)
+    , ((myMask, xK_BackSpace), kill)
+    -- Kill all
+    , ((myMask .|. shiftMask, xK_BackSpace), killAll)
     -- Launch a terminal
-    , ((mod4Mask, xK_Return), spawn myTerminal)
+    , ((myMask, xK_Return), spawn myTerminal)
     -- Swap the focused window and the master window
-    , ((mod4Mask .|. shiftMask, xK_Return), windows W.swapMaster)
+    , ((myMask .|. shiftMask, xK_Return), windows W.swapMaster)
     -- Manage scratchpad
-    , ((mod4Mask, xK_minus), scratchPad)
+    , ((myMask, xK_minus), scratchPad)
+    -- Mutt
+    , ((myMask, xK_m), mail)
     -- Toggle fullscreen
-    , ((mod4Mask, xK_f), sendMessage $ Toggle FULL)
+    , ((myMask, xK_f), sendMessage $ Toggle FULL)
     -- Recompile and restart xmonad
-    , ((mod4Mask, xK_q), spawn "stack ghc -v ~/.xmonad/xmonad.hs; ~/.xmonad/xmonad --restart")
+    , ((myMask, xK_q), spawn "stack ghc -v ~/.xmonad/xmonad.hs; ~/.xmonad/xmonad --restart")
+    -- Run pavucontrol
+    , ((myMask .|. controlMask, xK_m), spawn "pavucontrol")
     ]
     where
-        scratchPad = scratchpadSpawnActionTerminal ("xterm --title=scratchpad")
+        scratchPad = scratchpadSpawnActionTerminal myScratchpadTerminal
+        mail = spawn "alacritty -e neomutt"
 
 main = do
     xmonad $ def
         { terminal    = myTerminal
-        , modMask     = mod4Mask
+        , modMask     = myMask
         , manageHook  = myManageHook
         , layoutHook  = myLayout
         } `additionalKeys` myKeys

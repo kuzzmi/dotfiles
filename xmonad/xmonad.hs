1be8d23f0d985dff5740e4c40ad20769c43c344a
Igor Kuzmenko <igor@kuzzmi.com>
2018-11-16 12:09:22 +0200

    Updates xmonad.hs

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -11,6 +11,9 @@ import XMonad.Util.EZConfig (additionalKeys)
 import XMonad.Util.Run (spawnPipe)
 import XMonad.Util.Scratchpad
 
+import XMonad.Layout.NoFrillsDecoration
+import XMonad.Layout.ShowWName
+import XMonad.Layout.Decoration
 import XMonad.Layout.Fullscreen
 import XMonad.Layout.Grid
 import XMonad.Layout.MultiToggle
@@ -24,11 +27,83 @@ import qualified XMonad.StackSet as W
 
 myMask = mod4Mask -- win key
 
-myTerminal = "alacritty"
+------------------------------------------------------------------------}}}
+-- Colors, Fonts, & Themes                                              {{{
+---------------------------------------------------------------------------
 
+base03  = "#002b36"
+base02  = "#073642"
+base01  = "#586e75"
+base00  = "#657b83"
+base0   = "#839496"
+base1   = "#93a1a1"
+base2   = "#eee8d5"
+base3   = "#fdf6e3"
+yellow  = "#b58900"
+orange  = "#cb4b16"
+red     = "#dc322f"
+magenta = "#d33682"
+violet  = "#6c71c4"
+blue    = "#268bd2"
+cyan    = "#2aa198"
+green   = "#859900"
+
+gap     = 10
+border  = 0
+
+myNormalBorderColor  = base03
+myFocusedBorderColor = active
+
+active       = cyan
+activeWarn   = red
+inactive     = base02
+focusColor   = blue
+unfocusColor = base02
+
+myFont  = "-*-terminus-medium-*-*-*-*-160-*-*-*-*-*-*"
+myBigFont  = "-*-terminus-medium-*-*-*-*-240-*-*-*-*-*-*"
+
+topBarTheme= def
+    { fontName            = myFont
+    , inactiveBorderColor = base03
+    , inactiveColor       = base03
+    , inactiveTextColor   = base03
+    , activeBorderColor   = active
+    , activeColor         = active
+    , activeTextColor     = active
+    , urgentBorderColor   = red
+    , urgentTextColor     = yellow
+    , decoHeight          = 5
+    }
+
+myTabTheme = def
+    { fontName            = myFont
+    , activeColor         = active
+    , inactiveColor       = base02
+    , activeBorderColor   = active
+    , inactiveBorderColor = base02
+    , activeTextColor     = base01
+    , inactiveTextColor   = base1
+    }
+
+myShowWNameTheme = def
+    { swn_font    = "xft:Roboto:pixelsize=100:regular:antialias=true:hinting=true"
+    , swn_fade    = 0.25
+    , swn_bgcolor = base03
+    , swn_color   = active
+    }
+
+------------------------------------------------------------------------}}}
+-- Applications & Utilities                                             {{{
+---------------------------------------------------------------------------
+
+myTerminal           = "alacritty"
 myScratchpadTerminal = "urxvt"
+myStatusBar          = "xmobar -x0 $HOME/.xmonad/xmobarrc"
 
-myPlacement = fixed (0.5, 0.5) -- center of the screen
+myFocusFollowsMouse  = False
+myClickJustFocuses   = True
+myPlacement          = fixed (0.5, 0.5) -- center of the screen
 
 myManageHook =
   composeAll
@@ -50,18 +125,21 @@ manageScratchPad = scratchpadManageHook (W.RationalRect l t w h)
     t = d / g
     l = d / g
 
-myLayout = smartBorders . mkToggle (NOBORDERS ?? FULL ?? EOT) $ tiled ||| grid
+myLayout =
+  showWS (
+  smartBorders .
+  mkToggle (NOBORDERS ?? FULL ?? EOT)
+  $ ( addTopBar $ tiled )
+  ||| ( addTopBar $ grid ) )
   where
-    gaps = smartSpacingWithEdge 10
-    grid = gaps Grid
-        -- default tiling algorithm partitions the screen into two panes
-    tiled = gaps $ Tall nmaster delta ratio
-        -- The default number of windows in the master pane
-    nmaster = 1
-        -- Default proportion of screen occupied by master pane
-    ratio = 1 / 2
-        -- Percent of screen to increment by when resizing panes
-    delta = 1 / 25
+    addTopBar = noFrillsDeco shrinkText topBarTheme
+    showWS    = showWName' myShowWNameTheme
+    gaps      = smartSpacingWithEdge gap
+    grid      = gaps Grid
+    tiled     = gaps $ Tall nmaster delta ratio
+    nmaster   = 1
+    ratio     = 1 / 2
+    delta     = 1 / 25
 
 myKeys =
   [ ((myMask, xK_F2), spawn "inox --force-device-scale-factor=1.5") -- Launch browser
@@ -82,6 +160,7 @@ myKeys =
     -- Toggle fullscreen
   , ((myMask, xK_f), sendMessage $ Toggle FULL)
     -- Recompile and restart xmonad
+  , ((myMask, xK_0), spawn "i3lock -n")
   , ( (myMask, xK_q)
     , spawn
         "stack ghc -v ~/.xmonad/xmonad.hs; ~/.xmonad/xmonad-x86_64-linux --restart")
@@ -92,12 +171,46 @@ myKeys =
     scratchPad = scratchpadSpawnActionTerminal myScratchpadTerminal
     mail = spawn $ myTerminal ++ " -e neomutt"
 
-main =
-  xmonad $
-  def
-    { terminal = myTerminal
-    , modMask = myMask
-    , manageHook = myManageHook
-    , layoutHook = myLayout
+myLogHook h = do
+
+    -- following block for copy windows marking
+    -- copies <- wsContainingCopies
+    -- let check ws | ws `elem` copies =
+    --                pad . xmobarColor yellow red . wrap "*" " "  $ ws
+    --              | otherwise = pad ws
+
+    -- fadeWindowsLogHook myFadeHook
+    -- ewmhDesktopsLogHook
+    --dynamicLogWithPP $ defaultPP
+    dynamicLogWithPP $ def
+
+        { ppCurrent             = xmobarColor active "" . wrap "[" "]"
+        , ppTitle               = xmobarColor active "" . shorten 100
+        , ppVisible             = wrap "(" ")"
+        , ppUrgent              = xmobarColor red "" . wrap " " " "
+        -- , ppHidden              = check
+        , ppHiddenNoWindows     = const ""
+        , ppSep                 = xmobarColor red blue " : "
+        , ppWsSep               = " "
+        , ppLayout              = xmobarColor yellow ""
+        , ppOrder               = id
+        , ppOutput              = hPutStrLn h
+        -- , ppSort                = fmap
+        --                           (namedScratchpadFilterOutWorkspace.)
+        --                           (ppSort def)
+        , ppExtras              = [] }
+
+main = do
+    xmproc <- spawnPipe myStatusBar
+    xmonad $ myConfig xmproc
+
+myConfig p = def
+    { borderWidth       = border
+    , terminal          = myTerminal
+    , modMask           = myMask
+    , manageHook        = myManageHook
+    , layoutHook        = myLayout
+    , clickJustFocuses  = myClickJustFocuses
+    , focusFollowsMouse = myFocusFollowsMouse
     } `additionalKeys`
   myKeys

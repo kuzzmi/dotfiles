41a8744793ae118904ee327033a291918f76e473
Igor Kuzmenko <igor@kuzzmi.com>
2018-11-23 12:27:27 +0200

    Adds dotfiles command

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -28,6 +28,8 @@ alias m="neomutt"
 alias tmx="tmuxinator"
 alias tmxw="tmuxinator work"
 
+alias dotfiles="cd ~/.config/dotfiles"
+
 function gcoz() {
     git branch -a -vv --color=always | grep -v '/HEAD\s' |
         fzf --height 40% --ansi --multi --tac | sed 's/^..//' | awk '{print $1}' |
